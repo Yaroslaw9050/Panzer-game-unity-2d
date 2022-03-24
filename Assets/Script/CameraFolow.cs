ca393d26285721ab7557ab05f364da85dca59bfc
Yaroslaw9050 <ywgk0102@gmail.com>
2022-03-24 07:15:39 +0200

    Full first lesson

---
+++ Assets/Script/CameraFolow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFolow : MonoBehaviour
+{
+    [Range(0.01f, 0.1f)]
+    [SerializeField] private float cameraMoveSpeed;
+    [SerializeField] private Transform target;
+
+
+    private void LateUpdate()
+    {
+        Vector3 destination = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position =  Vector3.Lerp(transform.position, destination, cameraMoveSpeed);
+    }
+}
---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float tankSpeed;
+    private Rigidbody2D rb2d;
+    private bool isMovement;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Movement();
+    }
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb2d.velocity = transform.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb2d.velocity = -transform.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.MoveRotation(rb2d.rotation + 1f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.MoveRotation(rb2d.rotation - 1f);
+        }
+    }
+}
